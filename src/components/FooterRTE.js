665dd8df3277ff96af750b003bf3c7c6c3a843f4
RTEConIG <124293401+RTEConIG@users.noreply.github.com>
2025-05-09 23:15:42 -0300

    Create FooterRTE.js

---
+++ src/components/FooterRTE.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FooterRTE = () => {
+  return (
+    <footer className="text-center py-4 text-gray-600 text-sm mt-8">
+      Powered by <span className="font-bold">RTE</span>
+    </footer>
+  );
+};
+
+export default FooterRTE;
