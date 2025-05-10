612ec4859d451ab23ca361cecad34a8870de73a7
RTEConIG <124293401+RTEConIG@users.noreply.github.com>
2025-05-09 22:22:29 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BingoBoard from './components/BingoBoard';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <BingoBoard />
+    </div>
+  );
+};
+
+export default App;
+
+// DONE
