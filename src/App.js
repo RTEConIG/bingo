5cf7d1fab23e4693044edbfc702f46223f8b0f70
RTEConIG <124293401+RTEConIG@users.noreply.github.com>
2025-05-09 23:28:17 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import FooterRTE from './components/FooterRTE';
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100 py-8 flex flex-col">
-      <div className="flex-grow">
+    <div className="min-h-screen bg-gray-100 py-4 sm:py-6 flex flex-col">
+      <div className="flex-grow flex items-center justify-center">
         <BingoBoard />
       </div>
       <FooterRTE />
