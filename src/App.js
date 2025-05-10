3c97dd8837c5d43cfc9817fe672f138919e576f2
RTEConIG <124293401+RTEConIG@users.noreply.github.com>
2025-05-09 23:16:22 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import BingoBoard from './components/BingoBoard';
+import FooterRTE from './components/FooterRTE';
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100 py-8">
-      <BingoBoard />
+    <div className="min-h-screen bg-gray-100 py-8 flex flex-col">
+      <div className="flex-grow">
+        <BingoBoard />
+      </div>
+      <FooterRTE />
     </div>
   );
 };
